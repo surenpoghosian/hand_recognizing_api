7e3ebd1197e27384a3763433a754725db9e1f4d0
surenpoghosian <surenpoghosyan17@gmail.com>
2022-12-20 16:56:06 +0400

    commit

--- main.py
+++ main.py
@@ -8,17 +8,12 @@ import json
 import base64
 
 app = Flask(__name__)
-# First step is to initialize the Hands class an store it in a variable
 
-def readb64(uri):
-    print(1)
-    encoded_data = uri.split(',')[1]
-    print(2)
-    nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
-    print(3)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    print(4)
-    return img
+# def readb64(uri):
+#     encoded_data = uri.split(',')[1]
+#     nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
+#     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+#     return img
 
 
 @app.route('/recognize', methods=['POST'])
@@ -27,31 +22,13 @@ def get():
     decoded=data.decode("ISO-8859-1")
     decoded=json.loads(decoded)
     decoded=decoded["content"]
-    print(decoded)
-    # print(request.headers)
 
     mp_hands = mp.solutions.hands
 
-    # Now second step is to set the hands function which will hold the landmarks points
     hands = mp_hands.Hands(static_image_mode=True, max_num_hands=2, min_detection_confidence=0.3)
 
-    # Last step is to set up the drawing function of hands landmarks on the image
-    # mp_drawing = mp.solutions.drawing_utils
-
-
-    # Reading the sample image on which we will perform the detection
-    # sample_img = cv2.imread('media/sample5.jpg')
-    sample_img = readb64(decoded)
-
-
-    # sample_img = cv2.imread('media/hand_landmarks.png')
-
-    # Here we are specifing the size of the figure i.e. 10 -height; 10- width.
-    # plt.figure(figsize = [10, 10])
-
-    # Here we will display the sample image as the output.
-    # plt.title("Sample Image");plt.axis('off');plt.imshow(sample_img[:,:,::-1]);plt.show()
-
+    # sample_img = readb64(decoded)
+    sample_img = decoded
 
     results = hands.process(cv2.cvtColor(sample_img, cv2.COLOR_BGR2RGB))
 
